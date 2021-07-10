3f433f07b81392e5e5abb507f2cd04c83b37f74a
nalinbot <nalinnagar1@gmail.com>
2021-07-09 23:07:11 -0400

    Created model_utils.py :fire:
    Utility functions for building transformer

---
+++ model/model_utils.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import collections
+import unicodedata
+import os
+from bert import BertModelLayer
+from bert.loader import StockBertConfig, load_stock_weights
+from bert import bert_tokenization
+from bert.loader import map_to_stock_variable_name
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+
+    return pos * angle_rates
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
+    sines = np.sin(angle_rads[:, 0::2])
+    cosines = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = np.concatenate([sines, cosines], axis=-1)
+    pos_encoding = pos_encoding[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+  
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+
+    return mask
+  
+  
+def scaled_dot_product_attention(q, k, v, mask):
+    
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(attention_weights, v)
+    
+    return output, attention_weights
